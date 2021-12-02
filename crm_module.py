f8e0a5ffa9f43f9eb24368590e6403db92026ce4
matyas <matyasosv@gmail.com>
2021-12-02 15:10:39 +0000

    wip

--- crm_module.py
+++ crm_module.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from asciimatics.widgets import Frame, ListBox, Layout, Divider, Text, \
-    Button, TextBox, Widget
+    Button, TextBox, Widget, Label
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
@@ -17,18 +17,42 @@ class CRMView(Frame):
                                           screen.width * 2 // 3,
                                           hover_focus=True,
                                           can_scroll=False,
-                                          title="Contact Details",
+                                          title="CRM Module",
                                           reduce_cpu=True)
         # Save off the model that accesses the contacts database.
         # self._model = model
 
         # Create the form for displaying the list of contacts.
-        layout = Layout([1, 1, 1, 1])
+        layout = Layout([100])
         self.add_layout(layout)
-        layout.add_widget(Button("Add user", self._add), 0)
-        layout.add_widget(Button("List Customers", self._listall), 1)
-        layout.add_widget(Button("Edit Customer", self._edit ), 2)
-        layout.add_widget(Button("Delete Customer", self._delete), 3)
+        layout.add_widget(Label("Welcome to the CRM module! \n Please choose one of the following.",
+                                height=5,
+                                align="^"))
+        layout.add_widget(Divider())
+
+        layout1 = Layout([1])
+        self.add_layout(layout1)
+        layout1.add_widget(Button("Add user", self._add), 0)
+
+        layout2 = Layout([1])
+        self.add_layout(layout2)
+        layout2.add_widget(Button("List Customers", self._listall), 0)
+
+        layout3 = Layout([1])
+        self.add_layout(layout3)
+        layout3.add_widget(Button("Edit Customer", self._edit ), 0)
+
+        layout4 = Layout([1])
+        self.add_layout(layout4)
+        layout4.add_widget(Button("Delete Customer", self._delete), 0)
+
+        layout4.add_widget(Divider())
+
+        layout5 = Layout([1])
+        self.add_layout(layout5)
+        layout5.add_widget(Button("Quit", self._quit), 0)
+
+
         self.fix()
 
     def _add(self):
@@ -44,6 +68,9 @@ class CRMView(Frame):
     def _delete(self):
         return None
 
+    @staticmethod
+    def _quit():
+        raise StopApplication("User pressed quit")
 
 
 
